1b2ced2d93688c97b3a22270d0c151a7cd076e92
alviennn <alvien.rnp@gmail.com>
2023-05-26 17:31:19 +0700

    menambahkan fungsi traverse untuk mencetak catatan dengan urutan yang meningkat dan menampilkan pesan jika data kosong

--- double linkedlist/double linkedlist.cpp
+++ double linkedlist/double linkedlist.cpp
@@ -100,3 +100,16 @@ bool DoubleLinkedList::deletenode(int rollNo) {
 bool DoubleLinkedList::listEmpty() {
 	return (START == NULL);
 }
+
+void DoubleLinkedList::traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in ascending order of roll number are: " << endl;
+		node* currentnode = START;
+		while (currentnode != NULL) {
+			cout << currentnode->noMhs << " " << currentnode->name << endl;
+			currentnode = currentnode->next;
+		}
+	}
+}
