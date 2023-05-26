13c49c3a4d530d3cc4707a5226bcc8e3a6f319b4
alviennn <alvien.rnp@gmail.com>
2023-05-26 15:51:45 +0700

    menambahkan member untuk memasukkan nim dan nama

--- double linkedlist/double linkedlist.cpp
+++ double linkedlist/double linkedlist.cpp
@@ -25,5 +25,10 @@ public:
 };
 
 void DoubleLinkedList::addnode() {
-	
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
 }
