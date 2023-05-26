43676f51e78450c8c5dff77f68a75a68d15dbdce
alviennn <alvien.rnp@gmail.com>
2023-05-26 15:48:04 +0700

    menambahkan akses modifier public dengan member void dan bool

--- double linkedlist/double linkedlist.cpp
+++ double linkedlist/double linkedlist.cpp
@@ -13,4 +13,13 @@ public:
 class DoubleLinkedList {
 private:
 	node* START = NULL;
+public:
+	void addnode();
+	bool search(int rollNo, node** previous, node** current);
+	bool deletenode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
 };
